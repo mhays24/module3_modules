7265c0d6cf670a3cd8866c84c7dd6314b513a656
mhays24 <mhays24@basecampcodingacademy.org>
2023-11-09 16:20:05 -0600

    init commit

--- exercises/balloons/balloons.py
+++ exercises/balloons/balloons.py
@@ -3,28 +3,33 @@ class BalloonTooFull(Exception):
 
 
 class Balloon:
-    def pump(self):
+    def __init__(self) -> None:
+        self.capacity: int = 0
+        self.amount: int = 0
+
+    def pump(self) -> None:
         self.amount += 3
         if self.amount > self.capacity:
             raise BalloonTooFull("Pop!")
 
-    def release(self):
+    def release(self) -> None:
         self.amount -= 2
         self.amount = max(0, self.amount)
 
 
 class SwordBalloon(Balloon):
-    def __init__(self, capacity=5, amount=0):
-        self.capacity = capacity
-        self.amount = amount
+    def __init__(self) -> None:
+        super().__init__()
+        self.capacity: int = 5
 
 
 class DogBalloon(Balloon):
-    def __init__(self, capacity=7, amount=0):
-        self.capacity = capacity
-        self.amount = amount
+    def __init__(self) -> None:
+        super().__init__()
+        self.capacity: int = 7
+
 
 class TriforceBalloon(Balloon):
-    def __init__(self, capacity=11, amount=0):
-        self.capacity = capacity
-        self.amount = amount
+    def __init__(self) -> None:
+        super().__init__()
+        self.capacity: int = 11
--- exercises/contacts-list/app/models.py
+++ exercises/contacts-list/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.TextField()
+    email = models.TextField()
+    phone = models.TextField()
+    is_favorite = models.BooleanField(default=False)
+
+
+def create_contact(name, email, phone, is_favorite=False) -> Contact:
+    contact = Contact(name=name, email=email, phone=phone, is_favorite=is_favorite)
+    contact.save()
+    return contact
+
+def all_contacts() -> list:
+    return Contact.objects.all()
+
+def find_contact_by_name(name) -> Contact:
+    try:
+        return Contact.objects.get(name=name)
+    except: 
+        return None
+
+def favorite_contacts() -> list:
+    return Contact.objects.filter(is_favorite=True)
+
+def update_contact_email(name, new_email) -> Contact:
+    contact = Contact.objects.get(name=name)
+    contact.email = new_email
+    contact.save()
+    return contact
+
+def delete_contact(name) -> Contact:
+    contact = Contact.objects.get(name=name)
+    contact.delete()
+    return contact
